3ab08a574b4fd99a2164b3db73abe92af8c7cadb
Karliss <karlis3p70l1ij@gmail.com>
2024-05-19 22:22:04 +0300

    Qt 5.7

--- Makefile
+++ Makefile
@@ -24,15 +24,15 @@ endif
 #BASE_URL=http://www.mirrorservice.org/sites/download.qt-project.org
 BASE_URL=https://download.qt.io
 
-QT_VER_FULL=6.5.3
-QT_VER_SHORT=6.5
+QT_VER_FULL=6.7.0
+QT_VER_SHORT=6.7
 ifeq (${PLATFORM},win)
 QT_SRC_FILE=qt-everywhere-src-${QT_VER_FULL}.zip
-QT_SRC_MD5=af5879e459f028b0214fb7e34a968503
+QT_SRC_MD5=3923d6c0e9a5f986b0bd8678c97c14c8
 QT_SRC_URL=${BASE_URL}/official_releases/qt/${QT_VER_SHORT}/${QT_VER_FULL}/single/${QT_SRC_FILE}
 else
 QT_SRC_FILE=qt-everywhere-src-${QT_VER_FULL}.tar.xz
-QT_SRC_MD5=755db0527410df135609b51defa1a689
+QT_SRC_MD5=4468b457adb3a96294947e601fc877ca
 QT_SRC_URL=${BASE_URL}/official_releases/qt/${QT_VER_SHORT}/${QT_VER_FULL}/single/${QT_SRC_FILE}
 endif
 
--- build_windows.ps1
+++ build_windows.ps1
@@ -68,12 +68,12 @@ function DownloadAndCheckFile() {
 
 SetupVsEnv
 
-$version_base = "6.5"
-$version_full = "6.5.3"
+$version_base = "6.7"
+$version_full = "6.7.0"
 #$url = "https://download.qt.io/official_releases/qt/$version_base/$version_full/single/qt-everywhere-src-$version_full.zip"
 $url = "http://master.qt.io/archive/qt/$version_base/$version_full/single/qt-everywhere-src-$version_full.zip"
 $output = "qt-everywhere-src-$version_full.zip"
-$hash_expected = "86ac9e77f7a850d3f2e0b39763637636469bd3310ec2186234d18df17744bcba"
+$hash_expected = "9d5f98de73abc7411dcec2df729f64dd25f8021eff140155ec410950cad24fb5"
 $QT_SRC_DIR = "qt-everywhere-src-$version_full"
 $qt_build_dir = "$QT_SRC_DIR/build"
 $QT_PREFIX = "$PSScriptRoot/qt"
